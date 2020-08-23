8a72af08a4ba98235d57cf7bde1a899859d8ee9d
Simon Ziegler <simon@dioptra.tech>
2020-08-23 13:01:27 +0100

    First Entry

---
+++ RandomWalk/Point.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWalk
+{
+    public class Point
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+        public double R { get; private set; }
+        public double Theta { get; private set; }
+        public double Phi { get; private set; }
+
+        public static Point Cartesian(double x, double y, double z)
+        {
+            double r = 0;
+            double theta = 0;
+            double phi = 0;
+
+            try
+            {
+                r = Math.Sqrt(x * x + y * y + z * z);
+            }
+            catch { }
+
+            try
+            {
+                theta = Math.Atan(y / z);
+            }
+            catch { }
+
+            try
+            {
+                phi = Math.Acos(z / r);
+            }
+            catch { }
+
+            return new Point()
+            {
+                X = x,
+                Y = y,
+                Z = z,
+                R = r,
+                Theta = theta,
+                Phi = phi
+            };
+        }
+
+        public static Point Spherical(double r, double theta, double phi)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+
+            try
+            {
+                x = r * Math.Sin(theta) * Math.Cos(phi);
+            }
+            catch { }
+
+            try
+            {
+                y = r * Math.Sin(theta) * Math.Sin(phi);
+            }
+            catch { }
+
+            try
+            {
+                z = r * Math.Cos(theta);
+            }
+            catch { }
+
+            return new Point()
+            {
+                X = x,
+                Y = y,
+                Z = z,
+                R = r,
+                Theta = theta,
+                Phi = phi
+            };
+        }
+    }
+}
---
+++ RandomWalk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomWalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
